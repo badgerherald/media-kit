a28c8485923f044c6775d47bafcd55876030da16
Will Haynes <haynes24@gmail.com>
2014-12-30 19:05:18 -0600

    fleshing out structure. about section done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
 	$(window).resize(function() {
-		$('.jumbotron').height(window.innerHeight - 140);
+		$('.slide').height(window.innerHeight - 140);
 	}).resize();
 	
 });
