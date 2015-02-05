93ae2eefcab307421ddd0cf5f0b7acf9ea4e56bd
Will Haynes <haynes24@gmail.com>
2015-02-04 22:56:31 -0600

    Copy done, slides abstracted.

---
+++ slides/digital.php
@@ -0,0 +1,91 @@
+<div class="slide content-slide digital-slide" id="digital"
+	data-nav-background="#000000" data-nav-text-color="#ffffff">
+
+	<div class="slide-wrap">
+
+		<article>		
+
+			<header>
+				<h1>Digital Advertising</h1>
+			</header>
+			
+			<div class="left-pane">
+
+			<p>Advertising on <a href="http://badgerherald.com">badgerherald.com</a> is an economical and powerful way to reach the thousands of students who visit our site on a daily basis.</p>
+
+			<h2>How it works:</h2>
+
+			<p>Online ads are sold in qantities of 1,000 impressions spread out between a start and end date chosen by you. The number of purchased impressions determines how often the ad is displayed.</p>
+			
+			<h2>Rates:</h2>
+
+			<table class="rates">
+
+				<tbody>
+			  		<tr>
+			    		<td>Leaderboard</td>
+			    		<td class="price">$10/1k</td>
+			  		</tr>
+			  		<tr>
+			    		<td>Sidekick</td>
+			   			<td class="price">$10/1k</td>
+			  		</tr>
+			  		<tr>
+			    		<td>Billboard</td>
+			   			<td class="price">$12/1k</td>
+			  		</tr>
+			  	</tbody>
+
+				<tbody>
+
+			  		<tr>
+			    		<td>Animated</td>
+			    		<td class="price">add 15%</td>
+			  		</tr>
+			  		<tr>
+			    		<td colspan="2" class="explainer">Run .swf, .gif or video creative (no sound).</td>
+			  		</tr>
+			  		
+			  		<tr>
+			    		<td>Section Placement</td>
+			    		<td class="price">add 20%</td>
+			  		</tr>
+			  		<tr>
+			    		<td colspan="2" class="explainer">Choose sports, artsetc, or news placement.</td>
+			  		</tr>
+
+			  	</tbody>
+
+			</table>
+
+			<h2>Twitter Ads:</h2>
+
+			<p>We post an ad from our Badger Herald promo account and retweet it to our 12,000 followers. Optionally pay more to promote that tweet to thousands extra students.</p>
+
+			<table class="rates">
+
+				<tbody>
+			  		<tr>
+			    		<td>Promotional Tweet</td>
+						<td>$30 + promo</td>
+			  		</tr>
+			  		<tr>
+			  			<td colspan="2" class="explainer">Spend up to $50 to promote your tweet.</td>
+			  		</tr>
+			  	</tbody>
+			  </table>
+			</div>
+			
+			<div class="right-pane">
+			<img src="img/herald-ad-spots.png" />
+			</div>
+
+			
+
+		</article>
+
+		<div class="clearfix"></div>
+
+	</div>
+
+</div>
---
+++ slides/print.php
@@ -0,0 +1,212 @@
+<div class="slide content-slide print-slide" id="print"
+	data-nav-background="#eff4f6" data-nav-text-color="#000000">
+
+	<div class="slide-wrap">
+
+		<article>		
+
+			<header>
+				<h1>Print Advertising</h1>
+			</header>
+			
+			<div class="left-pane">
+				<img src="img/rate-card-print.png" width="100%" />
+			</div>			
+			
+			<div class="right-pane">
+
+				<p>With thousands of daily readers, advertising in print is a time proven way to get the word out.</p>
+
+				<h2>Circulation:</h2>
+
+				<p>11.5k papers distributed every Mon & Thur.</p>
+				<p>200 downtown newsstands on or near campus.</p>
+
+				<h2>How it works:</h2>
+
+				<p>Display ads are sold by column inches. Choose a width and height and multiply by the column rate. Don’t have artwork? Don’t worry. Our creative team can help convert previous artwork or design something completely new. <p> 
+
+				<h2>Rates:</h2>
+
+				<table class="rates">
+
+					<tbody>
+						<tr>
+				    		<td colspan="2" class="top-explainer">Column Inches:</td>
+				  		</tr>
+				  		<tr>
+				    		<td>National</td>
+				    		<td class="price">$12.75/inch</td>
+				  		</tr>
+				  		<tr>
+				    		<td>Agency</td>
+				   			<td class="price">$11/inch</td>
+				  		</tr>
+				  		<tr>
+				    		<td>Local</td>
+				   			<td class="price">$10.75/inch</td>
+				  		</tr>
+				  		<tr>
+				    		<td>Non-Profit</td>
+				   			<td class="price">$9.50/inch</td>
+				  		</tr>
+				  		<tr>
+				    		<td>UW</td>
+				   			<td class="price">$7.50/inch</td>
+				  		</tr>
+				  		<tr>
+				    		<td>Registered Student Org.</td>
+				   			<td class="price">$7.40/inch</td>
+				  		</tr>					  		
+				  	</tbody>
+
+					<tbody>
+						<tr>
+				    		<td colspan="2" class="top-explainer">Add Color:</td>
+				  		</tr>	
+				  		<tr>
+				    		<td>Full Color</td>
+				    		<td class="price">add $100</td>
+				  		</tr>
+				  	</tbody>
+
+					<tbody>
+						<tr>
+				    		<td colspan="2" class="top-explainer">Frequency:</td>
+				  		</tr>	
+				  		<tr>
+				    		<td colspan="2">10% off when purchasing three consecutive days.</td>
+				  		</tr>
+						<tr>
+				    		<td colspan="2" class="top-explainer">Credit:</td>
+				  		</tr>	
+				  		<tr>
+				    		<td colspan="2">5% off when payment is made with credit card.</td>
+				  		</tr>
+						<tr>
+				    		<td colspan="2" class="top-explainer">Guaranteed Placement:</td>
+				  		</tr>	
+				  		<tr>
+				    		<td colspan="2">Secure placement in news, sports or arts for 15%.</td>
+				  		</tr>
+						<tr>
+				    		<td colspan="2" class="top-explainer">Contract:</td>
+				  		</tr>	
+				  		<tr>
+				    		<td colspan="2">Sign a contract and recieve a discount. Clients committing to at least 250 column inches in a 12 month period recieve discounted rates.</td>
+				  		</tr>
+				  	</tbody>
+
+					<tbody>
+						<tr>
+				    		<td colspan="2" class="top-explainer">Creative design fees:</td>
+				  		</tr>	
+				  		<tr>
+				    		<td>1/8th page or smaller</td>
+				    		<td class="price">$20</td>
+				  		</tr>	
+				  		<tr>
+				    		<td>1/8th page or larger</td>
+				    		<td class="price">$30</td>
+				  		</tr>	
+				  	</tbody>
+
+				</table>
+
+
+				<h2>Artwork:</h2>
+				<p><b>Have your own artwork for the ad?</b> Take a look at our column-inch system below that specifies the precise dimensions of the artwork that you provide.</p>
+
+				<table class="rates">
+					<tbody>
+						<tr>
+				    		<td colspan="2" class="top-explainer">Column Inches</td>
+				  		</tr>	
+				  		<tr>
+				    		<td>1 column</td>
+				    		<td class="price">1.556"</td>
+				  		</tr>	
+				  		<tr>
+				    		<td>2 columns</td>
+				    		<td class="price">3.223"</td>
+				  		</tr>
+				  		<tr>
+				    		<td>3 columns</td>
+				    		<td class="price">4.889"</td>
+				  		</tr>	
+				  		<tr>
+				    		<td>4 column</td>
+				    		<td class="price">6.556"</td>
+				  		</tr>	
+				  		<tr>
+				    		<td>5 columns</td>
+				    		<td class="price">8.222"</td>
+				  		</tr>
+				  		<tr>
+				    		<td>6 columns</td>
+				    		<td class="price">9.889"</td>
+				  		</tr>
+				  	</tbody>
+				  </table>
+
+				  <h2>VIP Bundles</h2>
+
+				  <table class="rates">
+				  	<tbody>
+						<tr>
+				    		<td colspan="2" class="top-explainer">VIP Day Bundle ($500 - over $100 in savings):</td>
+				  		</tr>	
+				  		<tr>
+				    		<td colspan="2">¼ page full color print advertisement.</td>
+				  		</tr>
+				  		<tr>
+				    		<td colspan="2">1 Badger Herald endorsed tweet.</td>
+				  		</tr>
+						<tr>
+				    		<td colspan="2">10,000 impressions for a 300x250 Sidekick online advertisement </td>
+				  		</tr>	
+				  	</tbody>
+
+				  	<tbody>
+						<tr>
+				    		<td colspan="2" class="top-explainer">VIP Week Bundle ($1,000 - over $300 in savings) :</td>
+				  		</tr>	
+				  		<tr>
+				    		<td colspan="2">Two ¼ page full color print advertisements</td>
+				  		</tr>
+				  		<tr>
+				    		<td colspan="2">2 Badger Herald endorsed tweets</td>
+				  		</tr>
+						<tr>
+				    		<td colspan="2">50,000 impressions for a 300x250 Sidekick online advertisement</td>
+				  		</tr>	
+				  	</tbody>
+
+				  	<tbody>
+						<tr>
+				    		<td colspan="2" class="top-explainer">VIP Month Bundle ($1,500 - over $500 in savings):</td>
+				  		</tr>	
+				  		<tr>
+				    		<td colspan="2">Four ¼ full page color print advertisements.</td>
+				  		</tr>
+				  		<tr>
+				    		<td colspan="2">4 Badger Herald endorsed tweets.</td>
+				  		</tr>
+						<tr>
+				    		<td colspan="2">150,000 for a 300x250 Sidekick online advertisement.</td>
+				  		</tr>	
+				  	</tbody>
+
+				</table>
+
+			</div>
+			
+
+
+		</article>
+
+		<div class="clearfix"></div>
+
+	</div>
+
+</div>
